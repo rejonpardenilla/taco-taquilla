19e211e1cbb21f24c40914f794247ac989df7b60
Julián García <julian.gabus@gmail.com>
2017-09-29 12:59:37 -0500

    Se cambian los DataAccess Methods para usar SerializedObject

--- src/main/java/Elements/Base/DataAccessMethods.java
+++ src/main/java/Elements/Base/DataAccessMethods.java
@@ -3,5 +3,5 @@ package Elements.Base;
 import java.sql.SQLException;
 
 public interface DataAccessMethods {
-    public DataAccessMethods save() throws SQLException;
+    public SerializedObject save() throws SQLException;
 }
