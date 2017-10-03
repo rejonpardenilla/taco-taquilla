bf7ba3b8df3a997b1eed5ff21874591033772deb
Daniel Rejon <rejonpardenilla@codn.mx>
2017-10-03 07:52:56 -0500

    refactor zone view

--- src/main/java/Taquilla/View/ZoneView.java
+++ src/main/java/Taquilla/View/ZoneView.java
@@ -1,12 +1,11 @@
 package Taquilla.View;
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
 public class ZoneView {
-    private static final int ROWS = 20;
-    private static final int COLUMNS = 8;
+    private static final int ROWS = 8;
+    private static final int COLUMNS = 20;
     private static final char[] COLUMN_LETTERS = {'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
     private static final String WINDOW_DISPLAY_NAME = "Choose seats";
     private static final Dimension WINDOW_SIZE = new Dimension(1200, 800);
@@ -15,10 +14,10 @@ public class ZoneView {
     private Icon res = (UIManager.getIcon("OptionPane.errorIcon"));
 
     public ZoneView() {
-        JPanel panel = new JPanel(new GridLayout(COLUMNS, ROWS));
-        for (int column = 1; column <= COLUMNS; column++) {
-            for (int row = 1; row <= ROWS; row++) {
-                final JToggleButton button = new JToggleButton(COLUMN_LETTERS[column - 1] + "" + row);
+        JPanel panel = new JPanel(new GridLayout(ROWS, COLUMNS));
+        for (int row = 1; row <= ROWS; row++) {
+            for (int column = 1; column <= COLUMNS; column++) {
+                final JToggleButton button = new JToggleButton(COLUMN_LETTERS[row - 1] + "" + column);
                 performButtonAction(panel, button);
             }
         }
