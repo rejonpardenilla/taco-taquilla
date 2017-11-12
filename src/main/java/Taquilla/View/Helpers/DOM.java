1cad51a058d5e509f5e4fc1632efde2e5d157abc
Julián García <julian.gabus@gmail.com>
2017-11-12 15:44:42 -0600

    Adjustments to the DOM API (getValuesFrom and getCurrentValueFrom)

--- src/main/java/Taquilla/View/Helpers/DOM.java
+++ src/main/java/Taquilla/View/Helpers/DOM.java
@@ -1,6 +1,7 @@
 package Taquilla.View.Helpers;
 
 import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,7 +15,34 @@ public class DOM extends HashMap<String, Component> {
         return this.get(id);
     }
 
+    public Object invokeMethodOn(String id, String methodName){
+        java.lang.reflect.Method method;
+        try{
+            method = this.get(id).getClass().getMethod(methodName);
+            return method.invoke(this.get(id));
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Object invokeMethodOn(String id, String methodName, Object... args){
+        java.lang.reflect.Method method;
+        try{
+            Class<?>[] parameterTypes = new Class<?>[args.length];
+            for (int i = 0; i < args.length; i++){
+                parameterTypes[i] = args[i].getClass();
+            }
+            method = this.get(id).getClass().getMethod(methodName, parameterTypes);
+            return method.invoke(this.get(id), args);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public ArrayList<String> getAllIds(){
         return new ArrayList<>(this.keySet());
     }
+
 }
