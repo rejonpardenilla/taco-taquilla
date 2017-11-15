0adbfb9727d48c50f7250809ebdbdde8b1cbde00
Julián García <julian.gabus@gmail.com>
2017-11-15 14:14:36 -0600

    added FindByShow and FindByPlay to ticketDao

--- src/main/java/DataAccess/Implementations/TicketDao.java
+++ src/main/java/DataAccess/Implementations/TicketDao.java
@@ -2,10 +2,14 @@ package DataAccess.Implementations;
 
 import DataAccess.ConnectionFactory;
 import DataAccess.Interfaces.TicketDaoInterface;
+import Elements.Play;
+import Elements.Show;
 import Elements.Ticket;
 
 import java.math.BigDecimal;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicketDao extends BaseDao<Ticket> implements TicketDaoInterface {
 
@@ -48,11 +52,75 @@ public class TicketDao extends BaseDao<Ticket> implements TicketDaoInterface {
 
     }
 
+    @Override
+    public List<Ticket> findByShow(Show show) {
+        Connection connection = ConnectionFactory.getConnection();
+        Statement statement = null;
+
+        try {
+            statement = connection.createStatement();
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM play INNER JOIN show on play.id = show.play WHERE date='" + date + "'");
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT ticket.id as id, ticket.seating, ticket.returned, ticket.price, ticket.purchase " +
+                            "FROM " +
+                            "(ticket " +
+                            "INNER JOIN " +
+                            "(SELECT id as sid, show FROM seating) s " +
+                            "on ticket.seating = s.sid) WHERE s.show =" + show.getId());
+
+            ArrayList<Ticket> results = new ArrayList<Ticket>();
+            while(resultSet.next()){
+                Ticket result = extractFromResultSet(resultSet);
+                results.add(result);
+            }
+
+            return results;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Ticket> findByPlay(Play play){
+        Connection connection = ConnectionFactory.getConnection();
+        Statement statement = null;
+
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT tinfo.id as id, tinfo.seating as seating, tinfo.returned, tinfo.price, tinfo.purchase " +
+                        "FROM (" +
+                            "(SELECT id, seating as tseating, returned, price, purchase FROM ticket) t " +
+                            "INNER JOIN " +
+                                "((SELECT id as seating, show FROM seating) se " +
+                                "INNER JOIN " +
+                                "(SELECT id as showid, play FROM show) s " +
+                                "ON se.show = s.showid)" +
+                            "ON tseating = seating) tinfo " +
+                        "WHERE tinfo.play =" + play.getId());
+
+            ArrayList<Ticket> results = new ArrayList<Ticket>();
+            while(resultSet.next()){
+                Ticket result = extractFromResultSet(resultSet);
+                results.add(result);
+            }
+
+            return results;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
         TicketDao ticketDao = new TicketDao();
         SeatingDao seatingDao = new SeatingDao();
         PurchaseDao purchaseDao = new PurchaseDao();
         ShowDao showDao = new ShowDao();
+        PlayDao playDao = new PlayDao();
 
         Ticket ticket = new Ticket();
 
@@ -64,6 +132,8 @@ public class TicketDao extends BaseDao<Ticket> implements TicketDaoInterface {
         try {
             int id = ticketDao.insertTicket(ticket);
             ticket.setId(id);
+            List<Ticket> tickets = ticketDao.findByShow(showDao.findById(1));
+            System.out.println(tickets);
         } catch (SQLException e) {
             e.printStackTrace();
         }
