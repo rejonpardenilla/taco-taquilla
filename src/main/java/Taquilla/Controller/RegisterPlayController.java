1876e51883344f5951b9328e7d15097e4275f5f6
Daniel Rejon <rejonpardenilla@codn.mx>
2017-11-17 09:31:34 -0600

    fix zero price for tickets on migrations

--- src/main/java/Taquilla/Controller/RegisterPlayController.java
+++ src/main/java/Taquilla/Controller/RegisterPlayController.java
@@ -100,7 +100,7 @@ public class RegisterPlayController implements ActionListener {
             show.setDate(date);
             show.setTime(time);
             show.setPlay(play);
-            show.setPrice(BigDecimal.valueOf(0.00));
+            show.setPrice(BigDecimal.valueOf(100.00));
             show.setCancelled(false);
             shows.add(show);
         }
